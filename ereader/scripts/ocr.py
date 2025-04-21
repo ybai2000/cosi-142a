caf13d7db6e53e760336893dd02ff27f1a2b635a
EvanKeleti <evankeleti@brandeis.edu>
2025-04-21 11:26:59 -0400

    created project structure

---
+++ ereader/scripts/ocr.py
@@ -0,0 +1,8 @@
+from PIL import Image
+
+import pytesseract
+
+print(pytesseract.image_to_string(Image.open('../../test-image-for-recognition.png')))
+
+
+print('done')
--- ereader/speaker-microphone/audio.py
+++
@@ -1,4 +0,0 @@
-import sounddevice as sd
-
-# having issue with docker
-print(sd.query_devices())
