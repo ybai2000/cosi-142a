4a5ced8cc12c1fe423bf4c0c9ba8aba33d46c734
Yi Bai <156853544+ybai2000@users.noreply.github.com>
2025-04-25 15:39:38 -0400

    Revert "picolistener"

--- ereader/scripts/picolistener.py
+++ ereader/scripts/picolistener.py
@@ -1,10 +1,11 @@
 from enum import Enum
 import serial
+import json
 import threading
 import queue
-from enum import Enum
 
-class Signal(Enum):
+
+class Button(Enum):
     SELECT = 1
     BACK = 2
     UP = 3
@@ -14,6 +15,7 @@ class PicoListener:
     def __init__(self):
         self.PORT = "/dev/ttyACM0"
         self.BAUDRATE = 115200
+        self.DICT = ("select", "back", "up", "down")
         self.ser = serial.Serial(self.PORT, self.BAUDRATE, timeout=1)
         self.queue = queue.Queue()
         self.stop_event = threading.Event()  # Event to stop the thread
@@ -22,12 +24,16 @@ class PicoListener:
         while not self.stop_event.is_set():  # Check if the thread should stop
             if self.ser.in_waiting > 0:
                 try:
-                    message = int(self.ser.readline().decode('utf-8').strip())
-                    self.queue.put(Signal(message))
+                    message = json.loads(self.ser.readline().decode('utf-8').strip())
+                    for i in self.DICT:
+                        if message.get(i) == 1:  # Use `get` to avoid KeyError
+                            self.queue.put(i)
+                except json.JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e}")
                 except Exception as e:
                     print(f"Error reading signal: {e}")
 
-    def start_listening(self):
+    def listening(self):
         listener_thread = threading.Thread(target=self.read_signal)
         listener_thread.daemon = True
         listener_thread.start()
