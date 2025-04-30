067cca414f0d7ddc229d00f63a1cf7e11407c15e
yibai <ybai2000@brandeis.edu>
2025-04-30 16:13:47 +0000

    update init_deb.sh

--- init_deb.sh
+++ init_deb.sh
@@ -2,7 +2,9 @@
 
 # Update package list and install necessary system packages
 sudo apt update
-sudo apt install -y libasound-dev libportaudio2 libportaudiocpp0 portaudio19-dev gcc
+
+python3 -m venv reader-env
+source reader-env/bin/activate
 
 # Install Python packages from requirements.txt
 pip3 install -r requirements.txt
