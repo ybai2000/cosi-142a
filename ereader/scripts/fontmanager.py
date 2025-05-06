023e5e6c9ae6d476abeafdecd5862ef7b238d1ec
yibai <ybai2000@brandeis.edu>
2025-05-06 14:56:42 +0000

    update

--- ereader/scripts/fontmanager.py
+++ ereader/scripts/fontmanager.py
@@ -1,9 +1,20 @@
 import configparser
+import os
 from PIL import ImageFont
 
 
 class fontmanager():
     def __init__(self):
-        self.config = configparser.ConfigParse()
-        self.config.read("../fonts/fontconfig.cfg")
-        
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.path: str = os.path.join(script_dir, "..", "fonts")
+        self.available_fonts: list[str] = [
+            name for name in os.listdir(self.path)
+            if os.path.isdir(os.path.join(self.path, name))
+        ]
+        self.config = configparser.ConfigParser()
+
+    def get_font(self) -> ImageFont:
+        return self.font
+if __name__ == "__main__":
+    fm = fontmanager()
+    print(fm.available_fonts)
